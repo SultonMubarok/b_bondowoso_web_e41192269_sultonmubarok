0188208fdb4e3ac21afe61b4d4acbad85f2c334f
SultonMubarok <sultonresmi@gmail.com>
2021-03-24 23:56:31 +0700

    Tugas3_e41192269_sultonmubarok_golonganBbondowoso

---
+++ belajar_laravel/app/Http/Controllers/ManagementUserController.php
@@ -0,0 +1,47 @@
+<?php
+ 
+namespace App\Http\Controllers;
+ 
+use Illuminate\Http\Request;
+ 
+class ManagementUserController extends Controller
+{
+    public function index(){
+        return "Halo ini adalah method index, dalam controller ManagementUser";
+    }
+}
+{ 
+    public function create(){
+        return "Method ini digunakan menampilkan form untuk menambah data";
+    }
+}
+{
+public function index(){
+        return "Halo ini adalah method index, dalam controller ManagementUser";
+    }
+}
+{
+public function index(){
+        return "Halo ini adalah method index, dalam controller ManagementUser";
+    }
+}
+{
+public function index(){
+        return "Halo ini adalah method index, dalam controller ManagementUser";
+    }
+}
+{
+public function index(){
+        return "Halo ini adalah method index, dalam controller ManagementUser";
+    }
+}
+{
+public function update(Request $request, $id){
+        return "Halo ini adalah method index, dalam controller ManagementUser";
+    }
+}
+{
+public function destroy($id){
+        return "Method untuk menghapus data user dengan id";
+    }
+}
